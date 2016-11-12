1caad09f641f3d6712af8a9a9b774225f03df820
Imdutch21 <bartpklomp@gmail.com>
2016-11-12 23:12:22 +0100

    Setup basics

---
+++ src/sample/games/TwitchPlaysChess.java
@@ -0,0 +1,15 @@
+package sample.games;
+
+import javafx.stage.Stage;
+
+/**
+ * Created by bart on 12/11/2016.
+ */
+public class TwitchPlaysChess {
+
+    private Stage stage;
+
+    public TwitchPlaysChess(Stage stage){
+        this.stage = stage;
+    }
+}
---
+++ src/sample/views/GenericViews.java
@@ -0,0 +1,14 @@
+package sample.views;
+
+import javafx.scene.layout.Pane;
+
+import javax.swing.text.View;
+
+/**
+ * Created by bart on 12/11/2016.
+ */
+public class GenericViews {
+    public static Pane createChatView(){
+        return new Pane();
+    }
+}
