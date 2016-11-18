f6e34a4daed1653c8d1036348e6ba19544eb4958
imdutch21 <bartpklomp@gmail.com>
2016-11-18 23:19:56 +0100

    null check

--- src/sample/Bot.java
+++ src/sample/Bot.java
@@ -12,6 +12,7 @@ public class Bot extends PircBot {
     @Override
     protected void onMessage(String channel, String sender, String login, String hostname, String message) {
         super.onMessage(channel, sender, login, hostname, message);
-        Main.currentGame.onMessage(channel, sender, login, hostname, message);
+        if (Main.currentGame != null)
+            Main.currentGame.onMessage(channel, sender, login, hostname, message);
     }
 }
